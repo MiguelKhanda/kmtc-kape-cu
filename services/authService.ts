b9200a5a8bf10e92e4b850a892e5d1e09601075c
MiguelKhanda <miguelkhanda84@gmail.com>
2026-05-10 00:53:58 +0300

    f

--- services/authService.ts
+++ services/authService.ts
@@ -2,7 +2,7 @@ import api from '@/lib/axios'
 
 export const authService = {
     getCsrfToken: async () => {
-        return await api.get('api/csrf-cookie/');
+        return await api.get('/api/csrf-cookie/');
     },
     registerStudent:async(formData: any) => {
         return await api.post('/api/users/',formData);
