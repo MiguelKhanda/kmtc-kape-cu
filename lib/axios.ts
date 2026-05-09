0e7d04084b6b3f47c6c2bcf136fdf3dd9c322543
MiguelKhanda <miguelkhanda84@gmail.com>
2026-05-10 00:55:56 +0300

    f

--- lib/axios.ts
+++ lib/axios.ts
@@ -3,6 +3,8 @@ import axios from 'axios'
 const api = axios.create({
     baseURL: process.env.NEXT_PUBLIC_URL || 'https://kmtc-cu-backend.vercel.app',
     withCredentials: true,
+    xsrfCookieName: 'csrftoken',
+    xsrfHeaderName: 'X-CSRFToken',
     headers: {
         'Content-Type':'application/json',
     }
